5a9a4f1d3bfaa4d8a6843e334d4ab4c00af058b6
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2024-07-25 05:26:53 +0530

    Time: 722 ms (39.76%), Space: 37 MB (71.2%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,10 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair = [[p, s] for p, s in zip(position, speed)] #we pair up for efficient computation
-        pair.sort(reverse=True) #sort and start computation from car closest from the target in for loop
+        
         stack = [] # the length of stack will be unique fleets 
 
-        for p, s in pair: 
+        for p, s in sorted(pair)[::-1]: #sort and start computation from car closest from the target
             stack.append((target - p) / s) #append the time remaining
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
