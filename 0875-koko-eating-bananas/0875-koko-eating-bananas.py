76c0b05a159aab8a442cf07465a503e46f03304b
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2024-08-05 07:21:16 +0530

    Time: 251 ms (87.19%), Space: 18.2 MB (12.36%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,18 +1,29 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1 
-        r = max(piles) 
-        res = r 
+        # modified version of brute force
+        # do bin search on the possible solution then find the time taken by each pile
+
+
+        l = 1 # 1ban/hr is the min possible value
+        r = max(piles) #maximum possible value, above this there is no effect
+        res = r # assume max is the value and we will try to find the min value
+
+        # bin search to find appropriate K
         while l <= r:
-            k = ( l + r )//2
+            k = (l+r)//2
             timeTaken = 0
+            
+            # time-taken for the current K value
             for pile in piles:
                 timeTaken +=  math.ceil(pile/k)
+
+            # binarys search pointer update
             if(timeTaken <= h):
                 res = min(res, k)
                 r = k - 1
             else:
                 l = k + 1
+
         return res
 
         
