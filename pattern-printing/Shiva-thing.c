d7fdc721d209d7f9c1e249710853d8d66563257a
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-08-11 18:39:17 +0530

    brocamp fumigation learning

---
+++ pattern-printing/Shiva-thing.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min (int a , int b){
+    return a < b ? a : b;
+}
+
+
+
+int main() {
+   int n = 5;
+
+   for(int i = 0; i < 2*n -1; i++){
+    if(i<n){
+        for(int j=0 ; j<i; j++){
+            printf(" ");
+        }
+        for(int j=0; j<2*(n-i) -1 ; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    else{
+        int vrow = i - n;
+        for(int j=0; j< (n-1)-vrow-1; j++){
+            printf(" ");
+        }
+        for(int j=0; j< 2*(vrow+1) + 1; j++){
+            printf("*");
+        }
+        printf("\n");
+
+    }
+   }
+}
