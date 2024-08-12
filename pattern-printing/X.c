b8ad58e7378223aaacf377c7167c5ec052f0a661
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-08-12 19:13:32 +0530

    X

---
+++ pattern-printing/X.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+
+$   $
+ $ $
+  $
+ $ $
+$   $
+
+*/
+
+int main(int argc, char const *argv[])
+{
+    int n =5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if( i==j || j==n-i-1) printf("$");
+            else printf(" ");
+            
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
