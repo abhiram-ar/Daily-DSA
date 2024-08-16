99bb6b6578497346dde885361bb82999352fa79c
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-08-16 17:21:35 +0530

    completed day 2 daily taks"

--- C-array-problems/insert-inunsortedArray.c
+++ C-array-problems/insert-inunsortedArray.c
@@ -3,9 +3,11 @@ int main()
 {
     int arr[] = {1,2,4,55,-10,6,14,31};
     int size = sizeof(arr)/sizeof(arr[0]); 
+    
 
-    int position;
-    scanf("%d", &position);
+    int num,position;
+    printf("\nenter insert number and postion: ");
+    scanf("%d%d",&num, &position);
 
     
 
@@ -14,14 +16,14 @@ int main()
         if(i != position-1) arr[i+1] = arr[i];
         else{
             arr[i+1] = arr[i];
-            arr[i] = -1;
+            arr[i] = num;
             break;
         }
 
     }
 
     printf("\n");
-    for(int i=0; i<size+1;i++) printf("%d ", arr[i]);
+    for(int j=0; j<(size+1) ;j++) printf("%d ", arr[j]);
        
     return 0;
 }
