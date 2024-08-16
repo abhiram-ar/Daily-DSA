3cf2479b1996ead907637141a374886f408d585e
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2024-08-16 16:40:03 +0530

    Create leftsidedPiramid-realtive-row-concept.c

---
+++ pattern-printing/leftsidedPiramid-realtive-row-concept.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int relativerow = i;
+        if(i>n){
+            relativerow = (2*n-1) - i + 1 ;
+        }
+
+        for(int j=1; j<=relativerow;j++)printf("* ");
+        printf("\n");
+    }
+        
+    return 0;
+}
