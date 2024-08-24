46cf1c142ac26ea0edb4189db0793a2a60f68946
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-08-24 23:38:25 +0530

    q2 done

--- bootcamp-dailytask/day-8/arrays/q2-piramid-numbers.c
+++ bootcamp-dailytask/day-8/arrays/q2-piramid-numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
-        * 
-      * * * 
-    * * * * * 
-  * * * * * * * 
-* * * * * * * * * 
+        1 
+      1 2 3
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
 */
 int main()
 {
@@ -12,7 +12,7 @@ int main()
     for (int i = 1; i <=n; i++)
     {
         for(int j=1 ; j<= n-i; j++)printf("  ");
-        for(int j=1; j<=2*i-1; j++)printf("* ");
+        for(int j=1; j<=2*i-1; j++)printf("%d ", j);
         printf("\n");
     }
        
