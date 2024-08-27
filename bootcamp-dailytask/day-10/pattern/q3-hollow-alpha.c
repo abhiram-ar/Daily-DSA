522f02fb3689f3920b3217e41e4a3d462574bb12
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-08-27 18:56:15 +0530

    completed patters

---
+++ bootcamp-dailytask/day-10/pattern/q3-hollow-alpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+    A
+   A C
+  A   E
+ A     G
+ABCDEFGHI
+*/
+int main()
+{
+    int n=5;
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j=1; j<=n-i; j++)printf(" ");
+        for(int j=1; j<=2*i-1; j++){
+            if(j==1 || j==2*i-1 || i==n) printf("%c", 65+j-1);
+            else printf(" ");
+        }
+        printf("\n");
+    }
+       
+    return 0;
+}
