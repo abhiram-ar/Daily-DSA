9dd61784651d5c6f2ca5fe1a1380a1c8934676bd
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2024-09-25 23:44:52 +0530

    Time: 56 ms (30.89%), Space: 48.9 MB (32.09%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -4,13 +4,15 @@
  */
 var expect = function(val) {
     return {
-        toBe: (val2) => {
-            if (val !== val2) throw new Error("Not Equal");
+        toBe : (val2) => {
+            if(val !== val2) throw new Error("Not Equal");
             else return true;
+            
         },
-        notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal");
+        notToBe : (val2) => {
+            if(val === val2)throw new Error("Equal");
             else return true;
+            
         }
     }
 };
