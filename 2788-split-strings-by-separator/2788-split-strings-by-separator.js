60fe65e251cc5be8f926ef2b4471a3b3ff041af7
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2024-12-18 11:37:41 +0530

    Time: 19 ms (29.49%), Space: 57.6 MB (70.33%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.js
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.js
@@ -3,11 +3,10 @@
  * @param {character} separator
  * @return {string[]}
  */
-var splitWordsBySeparator = function (words, separator) {
+var splitWordsBySeparator = function(words, separator) {
     const res = []
     words.forEach(word => {
-        const chunks = word.split(separator)
-        console.log(chunks)
-        // chunksforEach(chunk => chunk !== "" ? res.push(chunk) : null)
-    })
-    return res
+        word.split(separator).forEach(chunk=> chunk!== "" && res.push(chunk))
+        })
+    return res.flat()
+};
