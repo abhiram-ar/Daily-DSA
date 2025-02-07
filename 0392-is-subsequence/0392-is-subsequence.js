8fc2b57c4b0de11f8862e515b3ff0b7e9ac46524
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-07 20:07:16 +0530

    Time: 0 ms (100%), Space: 48.2 MB (98.11%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -8,14 +8,8 @@ var isSubsequence = function (s, t) {
     let q = 0
 
     while (p < s.length && q < t.length) {
-        while (q < t.length) {
-            if (t[q] === s[p]) {
-                q++
-                p++
-                break
-            }
-            else q++
-        }
+        if (t[q] === s[p]) p++
+        q++
     }
     return p < s.length ? false : true
 };
