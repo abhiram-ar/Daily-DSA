7233cc124764a6a571310bccd26829f3a945b011
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-08 19:01:49 +0530

    Time: 0 ms (100%), Space: 50 MB (44.43%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    let l = 0
+    let r = nums.length - 1
+    let res = [-1, -1]
+    let mid
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+        if (nums[mid] === target && (mid === 0 || nums[mid - 1] < target)) {
+            res[0] = mid
+            break
+        }
+        if (nums[mid] >= target) r = mid - 1
+        else l = mid + 1
+    }
+    if(res[0] === -1) return res
+
+    r = nums.length - 1
+    mid = undefined
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+        if (nums[mid] === target && (mid === nums.length - 1 || nums[mid + 1] > target)) {
+            res[1] = mid
+            break
+        }
+        if (nums[mid] > target) r = mid - 1
+        else l = mid + 1
+    }
+    return res
+
+
+};
