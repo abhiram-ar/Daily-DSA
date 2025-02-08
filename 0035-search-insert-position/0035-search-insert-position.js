4078347d0a3f19b5302aeebb5f47dde7b44dd243
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-08 19:44:22 +0530

    Time: 0 ms (100%), Space: 49.3 MB (45.76%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let l = 0
+    let r = nums.length - 1
+    let mid
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+        if (nums[mid] === target) return mid
+        else if (nums[mid] > target) r = mid - 1
+        else l = mid + 1
+    }
+    return l 
+};
