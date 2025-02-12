1b2a32213950a2fdfa35441ff2d74ca47be7dd81
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-12 18:56:12 +0530

    Time: 2 ms (66.56%), Space: 51.1 MB (39.03%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    let words = s.split(" ").filter(word => word !== "" )
+    return words.reverse().join(" ")
+
+};
