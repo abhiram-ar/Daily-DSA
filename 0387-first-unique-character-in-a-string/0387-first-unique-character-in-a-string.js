5f0885cab23dcc022405372d5b649f4fa244ea5a
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-14 13:31:03 +0530

    Time: 94 ms (5.43%), Space: 78.9 MB (5.08%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+    let hashmap = {}
+    let orderq = []
+    for (let i = 0; i < s.length; i++) {
+        orderq.push([s[i], i])
+        hashmap[s[i]] = (hashmap[s[i]] || 0) + 1
+    }
+
+    for (let [char, index] of orderq) {
+        if (hashmap[char] === 1) return index
+    }
+    return -1
+};
