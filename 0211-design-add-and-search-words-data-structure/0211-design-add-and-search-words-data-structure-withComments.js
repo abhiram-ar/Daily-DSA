a6efaa4eca302026fbb88b04d37181aef9154086
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-22 17:49:52 +0530

    Time: 716 ms (37.03%), Space: 99.1 MB (32.63%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure-withComments.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor() {
+        this.links = {}
+        this.isEOW = false
+    }
+}
+
+var WordDictionary = function () {
+    this.root = new Node()
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function (word) {
+    let curr = this.root
+    for (let i = 0; i < word.length; i++) {
+        if (!curr.links[word[i]]) {
+            curr.links[word[i]] = new Node()
+        }
+        curr = curr.links[word[i]]
+    }
+    curr.isEOW = true
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function (word) {
+    function dfs(j, root) {
+        let curr = root
+        // i = j we dont want to start from start again
+        for (let i = j; i < word.length; i++) {
+            if (word[i] === ".") {
+                for (let node of Object.values(curr.links)) {
+                    // skip the current char and so dfs from next node
+                    if (dfs(i + 1, node)) {
+                        return true
+                    }
+                }
+                return false
+            }
+            else {
+                if (!curr.links[word[i]]) {
+                    return false
+                }
+                curr = curr.links[word[i]]
+            }
+        }
+        return curr.isEOW
+    }
+    return dfs(0, this.root)
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
