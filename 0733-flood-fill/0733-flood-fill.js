8b64fbb3700efb64f1b1b830f30e3deb595397f0
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-02-24 17:20:19 +0530

    Time: 1 ms (55.06%), Space: 57 MB (10.74%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -8,23 +8,20 @@
 var floodFill = function (image, sr, sc, color) {
     let visited = new Array(image.length).fill().map(() => [])
     let scolor = image[sr][sc]
-
-    let queue = [[sr, sc]]
+    let queue = new Deque([[sr, sc]])
     image[sr][sc] = color
-
-
-    let pixle
     let deltaR = [0, 1, 0, -1]
     let deltaC = [1, 0, -1, 0]
-    while (queue.length > 0) {
-        pixle = queue.shift()
+    let pixle
+    while (queue.size() > 0) {
+        pixle = queue.popFront()
         for (let i = 0; i < 4; i++) {
             let newR = pixle[0] + deltaR[i]
             let newC = pixle[1] + deltaC[i]
             if (newR < image.length && newR >= 0 && newC < image[0].length && newC >= 0 && !visited[newR][newC] && image[newR][newC] === scolor) {
                 image[newR][newC] = color
                 visited[newR][newC] = true
-                queue.push([newR, newC])
+                queue.pushBack([newR, newC])
             }
         }
     }
