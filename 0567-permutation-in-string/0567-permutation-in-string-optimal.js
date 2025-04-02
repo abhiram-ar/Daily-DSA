e116619fad775e84167e7542629417eb19698936
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-04-02 22:45:52 +0530

    Time: 24 ms (48.55%), Space: 60.6 MB (35.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string-optimal.js
+++ 0567-permutation-in-string/0567-permutation-in-string-optimal.js
@@ -13,6 +13,10 @@ var checkInclusion = function (s1, s2) {
         s2Freq[s2.charCodeAt(i) - "a".charCodeAt(0)]++
     }
 
+    // optimization: rather than looping through the frequency array,
+    // we can use a variable to hold the matches count, if mathces equal to 26 it is a permuatation
+
+    // first check the  initial matches
     let matches = 0
     for (let i = 0; i < 26; i++) {
         if (s2Freq[i] === s1Freq[i]) {
@@ -24,14 +28,18 @@ var checkInclusion = function (s1, s2) {
     for (let r = s1.length; r < s2.length; r++) {
         if (matches === 26) return true
 
+        // for right pointer
         let rIndex = s2.charCodeAt(r) - "a".charCodeAt(0)
         s2Freq[rIndex]++
         if (s2Freq[rIndex] === s1Freq[rIndex]) {
             matches++
         } else if (s2Freq[rIndex] === s1Freq[rIndex] + 1) {
+            // if the increment in frequncy by right pointer invalidated the previous match
             matches--
         }
 
+
+        // similar logic for left pointer
         let lIndex = s2.charCodeAt(l) - "a".charCodeAt(0)
         s2Freq[lIndex]--
         if (s2Freq[lIndex] === s1Freq[lIndex]) {
