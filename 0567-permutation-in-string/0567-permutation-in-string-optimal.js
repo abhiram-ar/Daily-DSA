38660b54814997a236abd7f2f3f288ceba9449d5
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-04-02 22:41:20 +0530

    Time: 18 ms (58.6%), Space: 58.2 MB (52.51%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string-optimal.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+    if (s1.length > s2.length) return false
+
+    const s1Freq = Array(26).fill(0)
+    const s2Freq = Array(26).fill(0)
+    for (let i in s1) {
+        s1Freq[s1.charCodeAt(i) - "a".charCodeAt(0)]++
+        s2Freq[s2.charCodeAt(i) - "a".charCodeAt(0)]++
+    }
+
+    let matches = 0
+    for (let i = 0; i < 26; i++) {
+        if (s2Freq[i] === s1Freq[i]) {
+            matches++
+        }
+    }
+
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+        if (matches === 26) return true
+
+        let rIndex = s2.charCodeAt(r) - "a".charCodeAt(0)
+        s2Freq[rIndex]++
+        if (s2Freq[rIndex] === s1Freq[rIndex]) {
+            matches++
+        } else if (s2Freq[rIndex] === s1Freq[rIndex] + 1) {
+            matches--
+        }
+
+        let lIndex = s2.charCodeAt(l) - "a".charCodeAt(0)
+        s2Freq[lIndex]--
+        if (s2Freq[lIndex] === s1Freq[lIndex]) {
+            matches++
+        } else if (s2Freq[lIndex] + 1 === s1Freq[lIndex]) {
+            matches--
+        }
+        l++
+    }
+
+    return matches === 26
+
+};
