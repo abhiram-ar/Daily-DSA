09d7a402b78c473f8c8fae174ecadfaabbabbca0
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-09-18 09:37:02 +0530

    Time: 5 ms (80.7%), Space: 57 MB (82.71%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters- optimal.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let hashmap = new Map()
+    let longest = 0
+    let l = 0
+    for (let r = 0; r < s.length; r++) {
+        if (hashmap.has(s[r]) && hashmap.get(s[r]) >= l) {
+            l = hashmap.get(s[r]) + 1
+        }
+        hashmap.set(s[r], r)
+        longest = longest > r - l + 1 ? longest : r - l + 1
+    }
+    return longest
+};
