f44a6da14ca425947719764904d5cc5b4b0b7682
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2025-10-17 10:13:06 +0530

    Time: 8 ms (62.17%), Space: 63.8 MB (80.16%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number-optimal-floyids-algorithm.ts
@@ -0,0 +1,16 @@
+function findDuplicate(nums: number[]): number {
+    let slow: number = 0
+    let fast: number = 0
+    while(true){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if(slow === fast) break
+    }
+
+    let slow2 = 0
+    while(true){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if(slow === slow2) return slow
+    }
+};
