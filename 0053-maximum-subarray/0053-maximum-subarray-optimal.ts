be4fba1fa828f75f7d733968286fb254781e5050
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-01-28 19:30:36 +0530

    Time: 1 ms (88.98%), Space: 64.8 MB (43.25%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray-optimal.ts
@@ -0,0 +1,11 @@
+function maxSubArray(nums: number[]): number {
+    let sum = nums[0]
+    let max = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        sum = Math.max(nums[i], sum + nums[i])
+
+        max = Math.max(sum, max)
+
+    }
+    return max
+};
