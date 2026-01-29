d1010e34806ed8fd59a59df4c6537b8b0e3855c8
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-01-29 17:27:50 +0530

    Time: 0 ms (100%), Space: 54.8 MB (81.13%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors-brute-fornce.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    let count = [0, 0, 0]
+    for (let i = 0; i < nums.length; i++) {
+        count[nums[i]]++
+    }
+
+    let p = 0
+    for (let i = 0; i < count.length; i++) {
+        let rep = 0
+        while (rep < count[i]) {
+            nums[p] = i
+            rep++
+            p++
+        }
+    }
+};
