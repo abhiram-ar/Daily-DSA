243edf4cd25a070f212deaba1b74b4f44c41e8aa
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-05 15:44:37 +0530

    Time: 10 ms (52.43%), Space: 64.4 MB (92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -1,20 +1,15 @@
 function merge(intervals: number[][]): number[][] {
     intervals.sort((a, b) => a[0] - b[0])
     const res = []
-    let prev = intervals[0]
+    res.push(intervals[0])
     let curr
     for (let i = 1; i < intervals.length; i++) {
         curr = intervals[i]
-        if(curr[0] > prev[1]){
-            res.push(prev)
-            prev = curr
+        if(curr[0] > res.at(-1)[1]){
+            res.push(curr)
         } else{
-            // edge: the prev closing range could be larger than the current closing range
-            prev[1] = Math.max(curr[1], prev[1])
+            res.at(-1)[1] = Math.max(curr[1], res.at(-1)[1])
         }
     }
-
-    // edge: the last range will not be pused to the result
-    res.push(prev)
     return res
 };
