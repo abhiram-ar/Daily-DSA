f2b85986f06f0a4e8c198ef681d463c2fbf1ad43
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-11 19:12:30 +0530

    Time: 4 ms (5.92%), Space: 58.3 MB (6.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,22 +1,35 @@
-function searchRange(nums: number[], target: number): number[] {
-    let ans = [-1, -1]
+function lastOccurance(arr: number[], k: number): number {
     let l = 0
-    let r = nums.length - 1
+    let r = arr.length - 1
+    let mid
     while (l <= r) {
-        let mid = Math.floor((l + r) / 2)
-        if (nums[mid] === target) {
-            let start = mid
-            while (nums[start] === target) ans[0] = start--
-
-            let end = mid
-            while (nums[end] === target) ans[1] = end++
-            return ans
-        } else if (target < nums[mid]) {
-            r = mid - 1
-        } else {
-            l = mid + 1
-        }
+        mid = Math.floor((l + r) / 2)
+        if (arr[mid] === k && (mid === arr.length - 1 || arr[mid + 1] > k)) return mid
+        else if (k < arr[mid]) r = mid - 1
+        else l = mid + 1
     }
+    return -1
+}
 
-    return ans
+function firstOccurance(arr: number[], k: number): number {
+    let l = 0
+    let r = arr.length - 1
+    let mid
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+
+        console.log(arr[mid])
+
+        if (arr[mid] === k && (mid === 0 || arr[mid - 1] < k)) return mid
+        else if (arr[mid] < k) l = mid + 1
+        else r = mid - 1
+    }
+    return -1
+}
+
+function searchRange(nums: number[], target: number): number[] {
+    const first = firstOccurance(nums, target)
+    if (first === -1) return [-1, -1]
+    const last = lastOccurance(nums, target)
+    return [first, last]
 };
