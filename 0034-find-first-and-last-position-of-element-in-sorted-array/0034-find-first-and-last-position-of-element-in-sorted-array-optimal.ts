5d94e87b5b62fbe18dd9d0a17a2e969ac7fe9a3d
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-11 19:12:50 +0530

    Time: 0 ms (100%), Space: 56.4 MB (46.34%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array-optimal.ts
@@ -0,0 +1,32 @@
+function lastOccurance(arr: number[], k: number): number {
+    let l = 0
+    let r = arr.length - 1
+    let mid
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+        if (arr[mid] === k && (mid === arr.length - 1 || arr[mid + 1] > k)) return mid
+        else if (k < arr[mid]) r = mid - 1
+        else l = mid + 1
+    }
+    return -1
+}
+
+function firstOccurance(arr: number[], k: number): number {
+    let l = 0
+    let r = arr.length - 1
+    let mid
+    while (l <= r) {
+        mid = Math.floor((l + r) / 2)
+        if (arr[mid] === k && (mid === 0 || arr[mid - 1] < k)) return mid
+        else if (arr[mid] < k) l = mid + 1
+        else r = mid - 1
+    }
+    return -1
+}
+
+function searchRange(nums: number[], target: number): number[] {
+    const first = firstOccurance(nums, target)
+    if (first === -1) return [-1, -1]
+    const last = lastOccurance(nums, target)
+    return [first, last]
+};
