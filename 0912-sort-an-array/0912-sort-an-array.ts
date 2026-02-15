3c2558ce9025a542a27fe0fe33a96d3679f37574
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-15 21:02:09 +0530

    Time: 67 ms (75.4%), Space: 74.7 MB (83.71%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.ts
+++ 0912-sort-an-array/0912-sort-an-array.ts
@@ -1,3 +1,30 @@
+function merge(arr, low, mid, high) {
+    let sorted = []
+    let p1 = low
+    let p2 = mid + 1
+    while (p1 <= mid && p2 <= high) {
+        if (arr[p1] <= arr[p2]) sorted.push(arr[p1++])
+        else sorted.push(arr[p2++])
+    }
+    while (p1 <= mid) sorted.push(arr[p1++])
+    while (p2 <= high) sorted.push(arr[p2++])
+
+    let p = low
+    for (let i = 0; i < sorted.length; i++) {
+        arr[p++] = sorted[i]
+    }
+    return arr
+}
+
+
+function mergeSort(arr, low, high) {
+    if (low >= high) return arr
+    const mid = Math.floor((low + high) / 2)
+    mergeSort(arr, low, mid)
+    mergeSort(arr, mid + 1, high)
+    return merge(arr, low, mid, high)
+}
+
 function sortArray(nums: number[]): number[] {
-    return nums.sort((a,b)=> a-b)
+    return mergeSort(nums, 0, nums.length - 1)
 };
