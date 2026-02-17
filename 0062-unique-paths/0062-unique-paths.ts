d3f552e110ed31c294e502edb082cb1b59a9d1d6
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-17 15:24:03 +0530

    Time: 1 ms (56.8%), Space: 54.8 MB (85.66%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,18 @@
+
+
+function uniquePaths(m: number, n: number): number {
+    if (m === 1 || n === 1) return 1
+
+    let matrix = Array.from({ length: m }, () => Array(n).fill(1))
+
+    for (let i = m - 2; i >= 0; i--) {
+        for (let j = n - 2; j >= 0; j--) {
+            matrix[i][j] = matrix[i + 1][j] + matrix[i][j + 1]
+        }
+    }
+
+    return matrix[0][0]
+
+
+
+};
