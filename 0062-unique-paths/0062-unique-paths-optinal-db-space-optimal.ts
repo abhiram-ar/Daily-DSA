820c2adb4de962207fd19e7253a28f58ba28064f
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-17 18:01:36 +0530

    Time: 0 ms (100%), Space: 55.9 MB (42.1%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths-optinal-db-space-optimal.ts
@@ -0,0 +1,23 @@
+function uniquePaths(m: number, n: number): number {
+    if (m === 1 || n === 1) return 1
+
+
+    let dp = Array(n).fill(0)
+    let temp = Array(n).fill(0)
+    let swap
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === 0 && j === 0) temp[j] = 1
+            else temp[j] = dp[j] + (temp[j - 1] ?? 0)
+        }
+        swap = dp
+        dp = temp
+        temp = swap
+    }
+
+    return dp[n - 1]
+
+
+
+};
