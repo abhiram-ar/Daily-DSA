e42264f12a43b75cda44c54d7f71fbb5ba02cba2
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-22 12:19:20 +0530

    Time: 30 ms (88.02%), Space: 72.5 MB (83.33%) - LeetHub

---
+++ 0015-3sum/0015-3sum-optimal.ts
@@ -0,0 +1,24 @@
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a,b)=> a-b)
+    let ans = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+        let j = i + 1
+        let k = nums.length - 1
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k]
+            if (sum < 0) {
+                j++
+            } else if (sum > 0) {
+                k--
+            } else {
+                ans.push([nums[i], nums[j], nums[k]])
+                j++
+                k--
+                while (j < k && nums[j] === nums[j - 1]) j++
+                while (j < k && nums[k] === nums[k + 1]) k--
+            }
+        }
+    }
+    return ans
+};
