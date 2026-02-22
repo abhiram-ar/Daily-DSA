07e2d30970de993bae27ebb38e121c01f9404113
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-22 10:39:24 +0530

    Time: 0 ms (100%), Space: 57.4 MB (90.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,16 +1,11 @@
 function majorityElement(nums: number[]): number {
-    let elem
     let count = 0
+    let elem = 0
     for (let i = 0; i < nums.length; i++) {
-        if (count === 0) {
-            elem = nums[i]
-        }
-        if (nums[i] === elem) {
-            count++
-        } else {
-            count--
-        }
-    };
-    return elem
+        if (!elem || count === 0) elem = nums[i]
 
-}
+        if (nums[i] === elem) count++
+        else count--
+    }
+    return elem
+};
