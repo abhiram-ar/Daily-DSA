be81b2fad77bb541564bea7850b48fb2465a27c4
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-25 10:25:39 +0530

    Time: 16 ms (74.05%), Space: 64.4 MB (58.47%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
@@ -1,12 +1,23 @@
 function subarraySum(nums: number[], k: number): number {
-    let sum = 0
+    let prefixMap = new Map()
     let count = 0
+    let cummSum = 0
     for (let i = 0; i < nums.length; i++) {
-        sum = 0
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j]
-            if (sum === k) count++
+        cummSum += nums[i]
+        if (cummSum === k) {
+            count++
         }
+
+        let rem = cummSum - k
+        if (prefixMap.has(rem)) {
+            count += prefixMap.get(rem)
+        }
+
+        // we need to longest subarry, so we avoid overriding the 
+        // earlier prexifSum idx with newer idx
+
+        prefixMap.set(cummSum, (prefixMap.get(cummSum) ?? 0) + 1)
+
     }
     return count
 };
