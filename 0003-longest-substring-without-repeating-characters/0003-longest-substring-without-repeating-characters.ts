e4e4305f7a985bc84ff3a54e1e0082be074252a1
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-25 19:09:11 +0530

    Time: 7 ms (53.95%), Space: 58.2 MB (87.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -3,14 +3,13 @@ function lengthOfLongestSubstring(s: string): number {
     let longest = 0
     let l = 0
     for (let r = 0; r < s.length; r++) {
-
         while (l <= r && map.has(s[r])) {
             map.delete(s[l])
             l++
         }
 
-        map.add(s[r])
         longest = Math.max(longest, r - l + 1)
+        map.add(s[r])
     }
     return longest
 };
