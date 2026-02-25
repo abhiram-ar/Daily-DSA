a843f0967095d4669edfc48950d4f6cb8b1a710f
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-02-25 10:53:14 +0530

    Time: 16 ms (74.05%), Space: 64.4 MB (61.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.ts
@@ -1,26 +1,19 @@
 function subarraySum(nums: number[], k: number): number {
-    // Map to store (prefixSum -> frequency)
     let prefixMap = new Map<number, number>();
-    
-    // Base case: A sum of 0 has occurred 1 time (empty subarray)
+
     prefixMap.set(0, 1);
-    
+
     let count = 0;
     let cummSum = 0;
-    
+
     for (let i = 0; i < nums.length; i++) {
         cummSum += nums[i];
-        
+
         let rem = cummSum - k;
-        
-        // If the remainder exists in the map, add its frequency to our count
-        if (prefixMap.has(rem)) {
-            count += prefixMap.get(rem)!;
-        }
-        
-        // Add the current cumulative sum to the map or increment its frequency
+        count += prefixMap.get(rem) ?? 0
+
         prefixMap.set(cummSum, (prefixMap.get(cummSum) || 0) + 1);
     }
-    
+
     return count;
 };
