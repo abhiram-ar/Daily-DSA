058f6bf473403b7c5d28069a9fe5120343f938d2
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-02 19:08:42 +0530

    Time: 50 ms (58.01%), Space: 58.7 MB (17.95%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function detectCycle(head: ListNode | null): ListNode | null {
+    if(!head) return null
+    
+    let slow = head
+    let fast = head 
+    let collided = false
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+        if(slow === fast) {
+            fast = head
+            while(slow !== fast){
+                slow = slow.next
+                fast = fast.next
+            }
+            return fast
+        }
+    }
+    return null
+
+};
