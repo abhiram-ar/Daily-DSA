e9363818879400544c8981c36226986bd516fb7d
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-06 23:30:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.ts
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.ts
@@ -15,27 +15,23 @@
 
 
 function copyRandomList(head: _Node | null): _Node | null {
-    const old2New = new Map<typeof head, typeof head>()
-    let curr = head
-    let newHead = new _Node(0)
-    let prev = newHead
-    while (curr) {
-        const newNode = new _Node(curr.val)
-        old2New.set(curr, newNode)
-        prev.next = newNode
-        prev = newNode
+    const old2New = new Map<_Node, _Node>()
+    old2New.set(null, null)
+    let oldCurr = head
 
-        curr = curr.next
+    while (oldCurr) {
+        if (!old2New.has(oldCurr)) old2New.set(oldCurr, new _Node(0))
+        const newNode = old2New.get(oldCurr)
+        newNode.val = oldCurr.val
+
+        if (!old2New.has(oldCurr.next)) old2New.set(oldCurr.next, new _Node(0))
+        newNode.next = old2New.get(oldCurr.next)
+
+        if (!old2New.has(oldCurr.random)) old2New.set(oldCurr.random, new _Node(0))
+        newNode.random = old2New.get(oldCurr.random)
+
+        oldCurr = oldCurr.next
     }
 
-    curr = head
-    while (curr) {
-        const randCurr = curr.random
-        const newCurr = old2New.get(curr) as _Node
-        const randNew = old2New.get(randCurr) as _Node
-        newCurr.random = randNew
-        curr = curr.next
-    }
-
-    return newHead.next
+    return old2New.get(head)
 };
