c3df4a633085f8de6698078a4f187e3be39c75fc
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-11 01:37:12 +0530

    Time: 2 ms (68.09%), Space: 65.5 MB (68.38%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxPathSum(root: TreeNode | null): number {
+    if (!root) return 0
+
+    let mx: [number] = [-Infinity]
+
+    function dfsSum(node: TreeNode | null, mx: [number]): number {
+        if (!node) return 0
+
+        let ls = Math.max(0, dfsSum(node.left, mx))
+        let rs = Math.max(0, dfsSum(node.right, mx))
+
+        let currSum = ls + rs + node.val
+        mx[0] = Math.max(mx[0], currSum)
+
+        return node.val + Math.max(ls, rs, 0) // 0 => slip -ve sum
+    }
+
+    dfsSum(root, mx)
+    return mx[0]
+};
