78806369a0704916958b2711dbeca47076fdfdd8
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-11 01:52:08 +0530

    Time: 0 ms (100%), Space: 56.1 MB (25.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.ts
+++ 0100-same-tree/0100-same-tree.ts
@@ -19,4 +19,5 @@ function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
     if (p.val !== q.val) return false
 
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+
 };
