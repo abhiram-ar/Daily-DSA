1e578ecf97d63582540c56b57ca4e6409defacac
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-11 03:06:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -13,41 +13,15 @@
  */
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-
-    function dfsHas(root: TreeNode | null, key: TreeNode): boolean {
-        if (!root) return false
-
-        if (root.val === key.val) return true
-        else if (root.val > key.val) {
-            let lr = dfsHas(root.left, key)
-            return lr
+    let curr = root
+    while(curr){
+        if(curr.val > p.val && curr.val > q.val){
+            curr = curr.left
+        } else if(curr.val < p.val && curr.val < q.val) {
+            curr = curr.right
         } else {
-            let rr = dfsHas(root.right, key)
-            return rr
+            // split has occured
+            return curr
         }
-
-        return false
     }
-
-    function dfs(node: TreeNode | null): TreeNode | null {
-        if (!node) return null
-
-        let lres = dfs(node.right)
-        if (lres) return lres
-
-        let rres = dfs(node.left)
-        if (rres) return rres
-
-        let fp = dfsHas(node, p)
-        let fq = dfsHas(node, q)
-
-        if (fp && fq) return node
-
-        return null
-
-    }
-
-    return dfs(root)
-
-
 };
