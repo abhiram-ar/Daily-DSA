56894d39a402cf62654b23c59ee31281d6e24435
Abhiram S Sajeev <82314439+abhiram-ar@users.noreply.github.com>
2026-03-17 13:28:01 +0530

    Time: 0 ms (100%), Space: 59.8 MB (78.28%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.ts
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.ts
@@ -13,11 +13,12 @@
  */
 
 function isValidBST(root: TreeNode | null): boolean {
-    function isValid(node: TreeNode | null, min: number, max: number) boolean{
+    function isValid(node: TreeNode | null, min: number, max: number): boolean {
         if (!node) return true
-        if (node.val >= min || node.val <= max) return false
+        if (!(node.val < max && node.val > min)) return false
 
         return isValid(node.left, min, node.val) &&
             isValid(node.right, node.val, max)
     }
+    return isValid(root, -Infinity, Infinity)
 };
